229d7839cff3c5687e828412771c6ed9c8bef1a0
nexxai <nexxai@gmail.com>
2024-03-31 20:00:20 -0600

    Adding Favicon and other metadata

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,44 +5,6 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      animation: {
-        marquee: "marquee var(--duration, 30s) linear infinite",
-      },
-      keyframes: {
-        marquee: {
-          "0%, 5%": {
-            opacity: 0,
-          },
-          "12.5%": {
-            opacity: 1,
-          },
-          "20%, 30%": {
-            opacity: 0,
-          },
-          "37.5%": {
-            opacity: 1,
-          },
-          "45%, 55%": {
-            opacity: 0,
-          },
-          "62.5%": {
-            opacity: 1,
-          },
-          "70%, 80%": {
-            opacity: 0,
-          },
-          "87.5%": {
-            opacity: 1,
-          },
-          "95%, 100%": {
-            opacity: 0,
-          },
-
-          to: {
-            transform: "translateX(-50%)",
-          },
-        },
-      },
       fontFamily: {
         heading: [
           "Mona Sans",
